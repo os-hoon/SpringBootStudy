5f786df3b80c771e723a444f4640b0f9a56e2b80
오승훈 <120100354+os-hoon@users.noreply.github.com>
2024-01-17 20:09:57 +0900

    update

--- firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ firstproject/src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,3 +1,5 @@
+
+//얘는 사용자가 입력한 데이터를 DB로 전달할때 주로 쓰는 클래스(update, create 등 DB에 데이터가 전달되어 저장되어야 할 때 쓰음)
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Article;
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,3 +1,6 @@
+
+//얘는 그냥 DB안의 데이터 보여주거나 그럴 때 주로 사용(Read)
+
 package com.example.firstproject.entity;
 
 import jakarta.persistence.*;
@@ -21,4 +24,10 @@ public class Article {
     private String content;
 
 
+    public void patch(Article article) {
+        if(article.title != null)
+            this.title = article.title;
+        if(article.content != null)
+            this.content = article.content;
+    }
 }
